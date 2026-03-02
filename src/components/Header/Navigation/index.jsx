bd78cd9967adffd174132daf0be4ecb2bb592340
Sneha Bramhe <snehabramhe03@gmail.com>
2026-03-02 16:53:51 +0530

    mega menu

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { HiMenuAlt1 } from "react-icons/hi";
 import { FaAngleDown } from "react-icons/fa6";
@@ -8,193 +8,228 @@ import CategoryPanel from './CategoryPanel';
 
 const Navigation = () => {
 
-     const [open, setOpen] = useState(false);
-    
-      const toggleDrawer = (newOpen) => () => {
+    const [open, setOpen] = useState(false);
+    const [active, setActive] = useState(null);
+
+    const toggleDrawer = (newOpen) => () => {
         setOpen(newOpen);
-      };
+    };
 
     return (
         <>
-           <nav className="py-2">
-            <div className="container flex items-center">
-                <div className="col_1 w-[20%]">
-                    <Button
-                        onClick={toggleDrawer(true)}
-                        sx={{
-                            padding: 0,
-                            color: "black",
-                            fontWeight: "bold",
-                            gap: 1,
-                            width: "100%"
-                        }}>
-                        <HiMenuAlt1 size={25} className='cursor-pointer'/>
-                        Shop by Categories
-                        <FaAngleDown size={15} className='ml-auto cursor-pointer' />
-                    </Button>
+            <nav className="py-2">
+                <div className="container flex items-center">
+                    <div className="col_1 w-[20%]">
+                        <Button
+                            onClick={toggleDrawer(true)}
+                            sx={{
+                                padding: 0,
+                                color: "black",
+                                fontWeight: "bold",
+                                gap: 1,
+                                width: "100%"
+                            }}>
+                            <HiMenuAlt1 size={25} className='cursor-pointer' />
+                            Shop by Categories
+                            <FaAngleDown size={15} className='ml-auto cursor-pointer' />
+                        </Button>
+                    </div>
+
+                    <div className="col_2 w-[60%] relative"
+                        onMouseEnter={() => setActive("fashion")}
+                        onMouseLeave={() => setActive(null)}>
+                        {/* Horizontal scroll wrapper */}
+                        <div className="overflow-x-auto no-scrollbar w-full">
+                            <ul className="flex items-center gap-2 pl-5 relative">
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/"
+                                        sx={{
+                                            fontWeight: 700,
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": {
+                                                color: "#ff5252",
+                                            },
+                                        }}
+                                    >
+                                        Home
+                                    </Button>
+                                </li>
+
+                                <li className="list-none relative"
+                                >
+                                    <Button
+                                        component={Link}
+                                        to="/fashion"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Fashion
+                                    </Button>
+
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/electronics"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Electronics
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/bags"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Bags
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/footwear"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Footwear
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/groceries"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Groceries
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/beauty"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": {
+                                                color: "#ff5252",
+                                            },
+                                        }}
+                                    >
+                                        Beauty
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/wellness"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Wellness
+                                    </Button>
+                                </li>
+
+                                <li className="list-none">
+                                    <Button
+                                        component={Link}
+                                        to="/jewellery"
+                                        sx={{
+                                            fontWeight: 700,
+                                            fontSize: "14px",
+                                            color: "rgba(0,0,0,0.8)",
+                                            textTransform: "none",
+                                            "&:hover": { color: "#ff5252" },
+                                        }}
+                                    >
+                                        Jewellery
+                                    </Button>
+                                </li>
+
+                            </ul>
+                        </div>
+
+
+
+                        <div
+                            className={`submenu absolute top-full left-0 bg-white w-full pb-2 shadow-md transition-all duration-500
+                                ${active === "fashion" ? "opacity-100 visible" : "opacity-0 invisible"}
+                            `}>
+                            <h6 className="text-primary pl-5 py-3 text-[14px] font-bold">Fashion</h6>
+                            <ul>
+                                <li className="list-none">
+                                    <Button sx={{ justifyContent: "start", textTransform: "none", paddingLeft: "20px", fontWeight: "600", "&:hover": { color: "rgba(0,0,0,0.8)" }, color: "rgba(0,0,0,0.5)" }}>Men</Button>
+                                </li>
+                                <li className="list-none">
+                                    <Button sx={{ justifyContent: "start", textTransform: "none", paddingLeft: "20px", fontWeight: "600", "&:hover": { color: "rgba(0,0,0,0.8)" }, color: "rgba(0,0,0,0.5)" }}>Women</Button>
+                                </li>
+                                <li className="list-none">
+                                    <Button sx={{ justifyContent: "start", textTransform: "none", paddingLeft: "20px", fontWeight: "600", "&:hover": { color: "rgba(0,0,0,0.8)" }, color: "rgba(0,0,0,0.5)" }}>Kids</Button>
+                                </li>
+                                <li className="list-none">
+                                    <Button sx={{ justifyContent: "start", textTransform: "none", paddingLeft: "20px", fontWeight: "600", "&:hover": { color: "rgba(0,0,0,0.8)" }, color: "rgba(0,0,0,0.5)" }}>Girls</Button>
+                                </li>
+                                <li className="list-none">
+                                    <Button sx={{ justifyContent: "start", textTransform: "none", paddingLeft: "20px", fontWeight: "600", "&:hover": { color: "rgba(0,0,0,0.8)" }, color: "rgba(0,0,0,0.5)" }}>Boys</Button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col_3 w-[20%] ml-auto">
+                        <p className="text-[14px] font-bold flex items-center justify-end gap-3"><GoRocket size={20} />Free International Delivery</p>
+                    </div>
+
                 </div>
+            </nav>
 
-                <div className="col_2 w-[60%]">
-                    <ul className="flex items-center gap-2 pl-5 overflow-x-auto no-scrollbar">
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/"
-                                sx={{
-                                    fontWeight: 700,
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": {
-                                        color: "#ff5252",
-                                    },
-                                }}
-                            >
-                                Home
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/fashion"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Fashion
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/electronics"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Electronics
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/bags"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Bags
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/footwear"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Footwear
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/groceries"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Groceries
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/beauty"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": {
-                                        color: "#ff5252",
-                                    },
-                                }}
-                            >
-                                Beauty
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/wellness"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Wellness
-                            </Button>
-                        </li>
-
-                        <li className="list-none">
-                            <Button
-                                component={Link}
-                                to="/jewellery"
-                                sx={{
-                                    fontWeight: 700,
-                                    fontSize: "14px",
-                                    color: "rgba(0,0,0,0.8)",
-                                    textTransform: "none",
-                                    "&:hover": { color: "#ff5252" },
-                                }}
-                            >
-                                Jewellery
-                            </Button>
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="col_3 w-[20%] ml-auto">
-                    <p className="text-[14px] font-bold flex items-center justify-end gap-3"><GoRocket size={20} />Free International Delivery</p>
-                </div>
-
-            </div>
-        </nav>
-
-        {/* Category pannel component */}
-        <CategoryPanel open={open} toggleDrawer={toggleDrawer}/>
+            {/* Category pannel component */}
+            <CategoryPanel open={open} toggleDrawer={toggleDrawer} />
         </>
     )
 }
